108a7282fc1362cf4ecb99c321aefba515be2734
saverioannpy <saverio.annunziata@outlook.com>
2023-11-07 11:32:04 +0100

    feat: utilizzo della variabile 'passeggeroSconto'

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 'use strict';
-const bigliettoPrezzoPerKm = 0.21; //Prezzo biglietto al km
-const bigliettoScontoMinorenni = 20; //Percentuale di sconto da applicare ai minorenni
-const bigliettoScontoOver65 = 40; //Percentuale di sconto da applicare agli over65
+
+const bigliettoPrezzoPerKm = 0.21; // Costante prezzo biglietto al km
+const bigliettoScontoMinorenni = 20; //Costante percentuale di sconto da applicare ai minorenni
+const bigliettoScontoOver65 = 40; //Costante percentuale di sconto da applicare agli over65
+let passeggeroSconto; //Variabile globale che verrà utilizzata per calcolare lo sconto a cui si ha diritto 
+let bigliettoPrezzo; //Variabile globale che verrà utilizzata per calcolare il prezzo del biglietto (dalla quale verranno sottratti eventuali sconti
+
 const bigliettoKm = Number(prompt('Inserire numero km da percorrere:')); //L'utente inserisce la distanza.
 console.log('[INPUT:]km inseriti: ' + bigliettoKm);
 const passeggeroEta = Number(prompt('Inserisci la tue età:'))//L'utente inserisce la sua età.
 console.log('[INPUT:]età inserita: ' + passeggeroEta);
-let bigliettoPrezzo = bigliettoKm * bigliettoPrezzoPerKm; //Viene calcolato il prezzo del biglietto (0.21€/km).
-if(passeggeroEta < 18){//Controllo se il passeggero è minorenne e applicazione sconto del 20%.
+if(!isNaN(bigliettoKm) && !isNaN(passeggeroEta)){ //Verifica che i dati inseriti siano SOLO numerici
+    bigliettoPrezzo = bigliettoKm * bigliettoPrezzoPerKm; //Viene calcolato il prezzo del biglietto (0.21€/km).
+    if(passeggeroEta < 18){//Controllo se il passeggero è minorenne e calcolo lo sconto del 20%.
 
-    bigliettoPrezzo = (bigliettoPrezzo - ((bigliettoPrezzo/100) * bigliettoScontoMinorenni)).toFixed(2);
+        passeggeroSconto = ((bigliettoPrezzo/100) * bigliettoScontoMinorenni);
 
-}else if(passeggeroEta > 65){ //Controllo se il passeggero è over65 e applicazione sconto del 40%.
+    }else if(passeggeroEta > 65){ //Controllo se il passeggero è over65 e calcolo lo sconto del 40%.
 
-    bigliettoPrezzo = (bigliettoPrezzo - ((bigliettoPrezzo/100) * bigliettoScontoOver65)).toFixed(2); 
-    
+        passeggeroSconto = ((bigliettoPrezzo/100) * bigliettoScontoOver65); 
+        
+    }
 }
+bigliettoPrezzo = bigliettoPrezzo - passeggeroSconto;
 console.log('[CALC:]prezzo biglietto: ' + (bigliettoPrezzo).toFixed(2)); //Console-log del prezzo Finale.
